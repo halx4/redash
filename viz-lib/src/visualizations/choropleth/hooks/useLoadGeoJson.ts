1f3b32262bf7f25253f85181a6c0aa3d6ac8ef4d
foivoschristoulakis <Christoulakis.Foivos@adlittle.com>
2021-01-28 15:12:43 +0000

    add console.logs

--- viz-lib/src/visualizations/choropleth/hooks/useLoadGeoJson.ts
+++ viz-lib/src/visualizations/choropleth/hooks/useLoadGeoJson.ts
@@ -7,13 +7,25 @@ import createReferenceCountingCache from "@/lib/referenceCountingCache";
 const cache = createReferenceCountingCache();
 
 export default function useLoadGeoJson(mapType: any) {
+  console.log("useLoadGeoJson(): w/ map type:")
+  console.log(mapType)
+
   const [geoJson, setGeoJson] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    console.log("useLoadGeoJson mapType changed. reloading...")
+
     const mapUrl = get(visualizationsSettings, `choroplethAvailableMaps.${mapType}.url`, undefined);
+    console.log("mapUrl:")
+    console.log(mapUrl)
+
+    console.log("viz settings:")
+    console.log(visualizationsSettings)
 
     if (isString(mapUrl)) {
+      console.log("map url is string")
+
       setIsLoading(true);
       let cancelled = false;
 
@@ -21,7 +33,15 @@ export default function useLoadGeoJson(mapType: any) {
       promise.then(({
         data
       }: any) => {
+        console.log("promise returned:")
+        console.log(data)
+        if (cancelled) {
+          console.log("promise request cancelled")
+        }
+
         if (!cancelled) {
+          console.log("promise request NOT cancelled")
+
           // @ts-expect-error ts-migrate(2345) FIXME: Argument of type 'object | null' is not assignable... Remove this comment to see the full error message
           setGeoJson(isObject(data) ? data : null);
           setIsLoading(false);
@@ -33,6 +53,8 @@ export default function useLoadGeoJson(mapType: any) {
         cache.release(mapUrl);
       };
     } else {
+      console.log("mapUrl is NOT string. setting geojson=null")
+
       setGeoJson(null);
       setIsLoading(false);
     }
